e3bc718f884de63af8171214f11125a9cbadbb0a
Aida Yrysbekova <aidiko20@gmail.com>
2019-06-25 17:48:12 -0400

    added the variables to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+alert("Press any key to get starter");
+
+alert("You have 5 guess.Guess 1-5");
+var countryName=["Spain","Malaysia", "Russia", "Zambia", "Italy","Argentina", "New Zealand","South Korea","Qatar","Switzerland"]
+var countryName = "Spain";
+var guess=prompt ("Which country is it?");
+
+for (i=0; i<5; i++);
+
+if (countryName==guess){
+    alert("You guessed correctly");
+} else {
+    guess=prompt("Please Try Again");
+}
