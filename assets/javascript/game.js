0592e6d33b6569982a863dcfbae2f91ed6bc1b8f
Aida Yrysbekova <aidiko20@gmail.com>
2019-06-26 17:26:07 -0400

    added image to starting page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,6 @@
-//Array 
-var countryName=["Spain","Malaysia", "Russia", "Zambia", "Italy","Argentina", "New Zealand","South Korea","Qatar","Switzerland"]
-var countryName = "_____";
-var wins = 0;
-var looses = 0;
-
-
-function myFunction() {
-    prompt("You pressed a key to start");
-  }
-//creating variables refrencing to HTML
-var directionsText = document.getElementById("directions-text");
-var userGuess = document.getElementById("userGuess");
-var 
-
-
+directionsText = document.getElementById("directions-text");
+document.onkeyup = function (event) {
+    var playerGuess = event.key;
+}
 
 
