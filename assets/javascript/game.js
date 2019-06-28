c375322128bd854bb720bc006cb3c270a8fadf88
Aida Yrysbekova <aidiko20@gmail.com>
2019-06-28 17:43:41 -0400

    adding the generate underscore function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,50 @@
-{
-    directionsText = document.getElementById("directions-text");
-    document.onkeyup = function (event) {
-        var playerGuess = event.key;
-        console.log(String.fromCharCode(event.which));
-    }
-}
-var question= ["Guess the country by flag"]
-
-var countries = [
-    "Spain", "Malaysia", "Russia", "Zambia", "Italy", "Switzerland", "South Korea", "New Zealand", "Qatar", "Argentina"
-]
-var countries = countries[Math.floor(Math.random() * countries.length)];
-
+// {
+//     directionsText = document.getElementById("directions-text");
+//     document.onkeyup = function (event) {
+//         var playerGuess = event.key;
+//         console.log(String.fromCharCode(event.which));
+//     }
+// }
+var question= ["Guess the country by flag"];
 var answerArray = [];
-for (var i = 0; i < countries.length; i++) {
- answerArray[i] = "_";
-}
-var remainingLetters = countries.length;
+// for (var i = 0; i < countries.length; i++) {
+//  answerArray[i] = "_";
+// }
+// var remainingLetters = countries.length;
 
 var Wins = 0;
 var Looses =0;
 var NoOfGuesses=5;
+var matchedLetters = [];
+var guessLeft = 0;
+var lettersOfTHEWord =[];
+var letterGuessed = null;
+
+function startGame(){
+    var countries = [
+        "SPAIN", "MALAYSIA", "RUSSIA", "ZAMBIA", "ITALY", "SWITZERLAND", "SOUTH KOREA", "NEW ZEALAND", "QATAR", "ARGENTINA"
+    ]
+    var countries = countries[Math.floor(Math.random() * countries.length)];
+
+    console.log("country: " +countries);
+    
+     lettersOfTHEWord = countries.split("");
+
+    console.log("letters: " +lettersOfTHEWord);
+    generateUNDERSCORES();
+    setTotalGuess();
+}
+function generateUNDERSCORES(){ 
+    var wordView = "";
+    for(var i=0; i<lettersOfTHEWord.length;i++){
+        if(matchedLetters.indexOf(lettersOfTHEWord[i]) !== -1){
+            wordView += lettersOfTHEWord[i];
+
+        }
+        else{
+            wordView += "&nbsp;_&nbsp";
+        }
+    }
+    document.querySelector("#currentWord").innerHTML = wordView;
+}
+
