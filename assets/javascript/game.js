886fbe9427b1e4695bdb082977cbd1b3282f1c3f
Aida Yrysbekova <aidiko20@gmail.com>
2019-06-29 18:03:40 -0400

    added jumbotron with image on background

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
-// {
-//     directionsText = document.getElementById("directions-text");
-//     document.onkeyup = function (event) {
-//         var playerGuess = event.key;
-//         console.log(String.fromCharCode(event.which));
-//     }
-// }
-var question= ["Guess the country by flag"];
+{
+    directionsText = document.getElementById("directions-text");
+    document.onkeyup = function (event) {
+        var playerGuess = event.key;
+        console.log(String.fromCharCode(event.which));
+    }
+}
+var question = ["Guess the country by flag"];
 var answerArray = [];
 // for (var i = 0; i < countries.length; i++) {
 //  answerArray[i] = "_";
@@ -13,76 +13,107 @@ var answerArray = [];
 // var remainingLetters = countries.length;
 
 var Wins = 0;
-var Looses =0;
-var NoOfGuesses=5;
+var Looses = 0;
+var NoOfGuesses = 5;
 var matchedLetters = [];
 var guessLeft = 0;
-var lettersOfTHEWord =[];
+var lettersOfTHEWord = [];
 var letterGuessed = null;
 
-function startGame(){
+function startGame() {
     var countries = [
         "SPAIN", "MALAYSIA", "RUSSIA", "ZAMBIA", "ITALY", "SWITZERLAND", "SOUTH KOREA", "NEW ZEALAND", "QATAR", "ARGENTINA"
     ]
     var countries = countries[Math.floor(Math.random() * countries.length)];
 
-    console.log("country: " +countries);
-    
-     lettersOfTHEWord = countries.split("");
+    console.log("country: " + countries);
 
-    console.log("letters: " +lettersOfTHEWord);
+    lettersOfTHEWord = countries.split("");
+
+    console.log("letters: " + lettersOfTHEWord);
     generateUNDERSCORES();
     setTotalGuess();
 }
-function generateUNDERSCORES(){ 
+function generateUNDERSCORES() {
     var wordView = "";
-    for(var i=0; i<lettersOfTHEWord.length;i++){
-        if(matchedLetters.indexOf(lettersOfTHEWord[i]) !== -1){
+    for (var i = 0; i < lettersOfTHEWord.length; i++) {
+        if (matchedLetters.indexOf(lettersOfTHEWord[i]) !== -1) {
             wordView += lettersOfTHEWord[i];
 
         }
-        else{
+        else {
             wordView += "&nbsp;_&nbsp";
         }
     }
     document.querySelector("#currentWord").innerHTML = wordView;
 }
-function setTotalGuess(){
+function setTotalGuess() {
     NoOfGuesses = lettersOfTHEWord.length + 5;
     guessLeft = NoOfGuesses;
     document.querySelector("#guessLeft").innerHTML = guessLeft;
-    
-    }
-    startGame();
-    
-    document.onkeyup = function(event){
+
+}
+startGame();
+
+document.onkeyup = function (event) {
     letterGuessed = String.fromCharCode(event.which).toUpperCase();
     updatePage(letterGuessed);
+}
+function updatePage(letter) {
+    if (guessLeft == 0) {
+        restartGame();
     }
-    function updatePage(letter){
-    if(guessLeft == 0){
-    restartGame();
+    else {
+        updateGuesses(letter);
+        updateCorrectMatch(letter);
+        generateUNDERSCORES();
+        updateWins(Number);
+        updateLoss(Number);
     }
-    else{
-    updateGuesses(letter);
-    updateCorrectMatch(letter);
-    generateUNDERSCORES();
-    updateWins();
-    updateLoss();
-    }
-    }
-    function updateGuesses(letter){
-        if(letterGuessed.indexOf(letter) === -1 && lettersOfTHEWord.indexOf(letter)=== -1){
+}
+function updateGuesses(letter) {
+    if (letterGuessed.indexOf(letter) === -1 && lettersOfTHEWord.indexOf(letter) === -1) {
         letterGuessed.push(letter);
         guessLeft--;
         document.querySelector("#guessLeft").innerHTML = guessLeft;
+    }
+}
+function updateCorrectMatch(letter) {
+    for (var i = 0; i < lettersOfTHEWord.length; i++) {
+        if (letter === lettersOfTHEWord[i] && matchedLetters.indexOf(letter) === -1) {
+            matchedLetters.push(letter);
         }
     }
-    function updateCorrectMatch(letter){
-        for(var i=0; i<lettersOfTHEWord.length; i++){
-        if(letter === lettersOfTHEWord[i] && matchedLetters.indexOf(letter) === -1){
-        matchedLetters.push(letter);
-        }
-        }
-        }
-    
+}
+function updateWins() {
+    var win;
+    if (matchedLetters.length === 0) {
+        win = false;
+    }
+    else {
+        win = true;
+    }
+    if (win) {
+        wins++;
+        document.querySelector("#Wins").innerHTML = wins;
+    }
+}
+function updateLoss() {
+    var loss;
+    if (matchedLetters.length === -1) {
+        loss = false;
+    }
+    else {
+        loss = true;
+    }
+    if (loss) {
+        loss++;
+        document.querySelector("#Losses").innerHTML = loss;
+    }
+}
+ function restartGame(){
+     var startGame;
+     if (matchedLetters.length === wins){
+         prompt("You won the game!")
+     }
+ }
